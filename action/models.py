00cae4c3e30a46cbd985620ce0ffbeb0da426751
Amirhosein <ah.rezaie07@gmail.com>
2025-08-16 18:33:27 -0700

    create models and hash password

--- action/models.py
+++ action/models.py
@@ -1,3 +1,86 @@
 from django.db import models
+from core.models import (Users, Tasks, Projects)
+from django.utils import timezone
 
-# Create your models here.
+
+# tag on the tasks by users(foremans)
+class Tags(models.Model):
+    class Titles(models.TextChoices):
+        TODO = "ToDo"
+        INPROGRESS = "InProgress"
+        DONE = "Done"
+
+    user = models.ForeignKey(
+        Users, null=False, blank=False, related_name='user_tag'
+    )
+    task = models.ForeignKey(
+        Tasks, null=False, blank=False, related_name='tag_task'
+    )
+    title = models.CharField(
+        choices=Titles, null=False, blank=False, default=Titles.TODO
+    )
+    date = models.DateField(
+        auto_now_add=True
+    )
+    time = models.TimeField(
+        auto_now_add=True
+    )
+
+    class Meta:
+        db_table = "Tags"
+
+    def __str__(self):
+        return f"{self.user} -> {self.task}"
+
+
+# set score for users(foreman)
+class Scores(models.Model):
+    score = models.IntegerField(
+        choices=[n for n in range(1, 6)], null=False, blank=False
+    )
+    user = models.ForeignKey(
+        Users, on_delete=models.CASCADE, null=False, blank=False
+    )
+    task = models.ForeignKey(
+        Tasks, null=False, blank=False, on_delete=models.CASCADE
+    )
+
+    class Meta:
+        db_table = 'Scores'
+
+    def __str__(self):
+        return f"{self.user} -> {self.task} => {self.score}"
+
+
+# members of every projects model
+class ProjectMembers(models.Model):
+    class Status(models.TextChoices):
+        IN_PROJECT = "InProject"
+        LEFT_PROJECT = "LeftProject"
+
+    member = models.ForeignKey(
+        Users, on_delete=models.CASCADE, null=False, blank=False
+    )
+    project = models.ForeignKey(
+        Projects, on_delete=models.CASCADE, null=False, blank=False
+    )
+    join_date = models.DateField(
+        auto_now_add=True
+    )
+    left_date = models.DateField(
+        null=True, blank=True
+    )
+    status = models.CharField(
+        max_length=100, choices=Status, default=Status.IN_PROJECT, null=False, blank=False
+    )
+
+    class Meta:
+        db_table = "Project_Members"
+
+    def __str__(self):
+        return f"{self.member} -> {self.project}"
+
+    def save(self, *args, **kwargs):
+        if self.status == self.Status.LEFT_PROJECT and self.left_date in None:
+            self.left_date = timezone.now().date()
+        super().save(*args, **kwargs)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,95 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Users(AbstractUser):
+    class Roles(models.TextChoices):
+        MANAGER = "manager"
+        FOREMAN = "foreman"
+
+    first_name = models.CharField(
+        max_length=100, null=False, blank=False
+    )
+    last_name = models.CharField(
+        max_length=100, null=False, blank=False
+    )
+    role = models.CharField(
+        choices=Roles, null=False,
+        blank=False, default=Roles.FOREMAN
+    )
+    phone = models.CharField(
+        max_length=100, null=False, blank=False
+    )
+
+    class Meta:
+        db_table = 'Users'
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def save(self, *args, **kwargs):
+        if self.password and not self.password.startswith('pbkdf2_'):
+            self.set_password(self.password)
+        return super().save(*args, **kwargs)
+
+
+# projects
+class Projects(models.Model):
+    class Status(models.TextChoices):
+        IN_PROGRESS = "In_Progress"
+        DONE = "Done"
+
+    title = models.CharField(
+        max_length=100, null=False,
+        blank=False, unique=True
+    )
+    description = models.TextField(
+        null=True, blank=True
+    )
+    user = models.ForeignKey(
+        Users, on_delete=models.CASCADE, null=False, blank=False
+    )
+    status = models.CharField(
+        max_length=100, choices=Status,
+        default=Status.IN_PROGRESS, null=False, blank=False
+    )
+
+    class Meta:
+        db_table = "Projects"
+
+    def __str__(self):
+        return self.title
+
+
+class Tasks(models.Model):
+    class Status(models.TextChoices):
+        PICKED = "picked",
+        PENDING = 'pending'
+
+    title = models.CharField(
+        max_length=100, null=False, blank=False
+    )
+    project = models.ForeignKey(
+        Projects, null=False, blank=False, on_delete=models.CASCADE
+    )
+    description = models.TextField(
+        null=True, blank=True
+    )
+    user = models.ForeignKey(
+        Users, on_delete=models.CASCADE, null=False, blank=False, related_name='user_task'
+    )
+    level = models.IntegerField(
+        choices=[n for n in range(1, 6)], null=False, blank=False
+    )
+    deadline = models.DateField(
+        null=False, blank=False
+    )
+    status = models.CharField(
+        choices=Status, null=False, blank=False, default=Status.PENDING
+    )
+
+    class Meta:
+        db_table = "Tasks"
+
+    def __str__(self):
+        return self.title
