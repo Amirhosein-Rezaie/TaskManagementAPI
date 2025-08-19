3f91a17f1137f9f24e3545e99f4e241ac1a1d30c
Amirhosein <ah.rezaie07@gmail.com>
2025-08-19 00:30:05 -0700

    add simple settings of rest_framefork

--- TaskManagementAPI/settings.py
+++ TaskManagementAPI/settings.py
@@ -134,3 +134,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.Users'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'TaskManagementAPI.pagination.DynamicPagination',
+}
