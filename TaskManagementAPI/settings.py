cbb5c24539dc3eb54ce3134994aeb5d7562c029b
Amirhosein <ah.rezaie07@gmail.com>
2025-08-24 22:29:11 -0700

    add defualt permission

--- TaskManagementAPI/settings.py
+++ TaskManagementAPI/settings.py
@@ -141,4 +141,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'TaskManagementAPI.pagination.DynamicPagination',
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
 }
